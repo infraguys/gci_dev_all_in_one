a11f77274a4d2b4c853a7e2b4908c252d2e71227
George Melikov <mail@gmelikov.ru>
2025-07-07 15:34:03 +0300

    Initial release

---
+++ genesis/images/bootstrap.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+
+set -eu
+set -x
+set -o pipefail
+
+EL_PATH="/opt/stand/genesis_core"
+
+cd $EL_PATH
+source /opt/.venv/bin/activate
+genesis bootstrap -i output/genesis-core.raw -f -m core --memory 3000
+sudo virsh autostart genesis-core-bootstrap
+
+while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' 10.20.0.2:11010)" != "200" ]]; do
+    sleep 5;
+done
+
+ADMIN_TOKEN=$(curl --location 'http://10.20.0.2:11010/v1/iam/clients/00000000-0000-0000-0000-000000000000/actions/get_token/invoke' \
+    --header 'Content-Type: application/x-www-form-urlencoded' \
+    --data-urlencode 'grant_type=password' \
+    --data-urlencode 'username=admin' \
+    --data-urlencode 'password=admin' \
+    --data-urlencode 'client_id=GenesisCoreClientId' \
+    --data-urlencode 'client_secret=GenesisCoreClientSecret' \
+    --data-urlencode 'scope=' \
+    --data-urlencode 'ttl=31536000' | jq .access_token -r)
+
+echo $ADMIN_TOKEN > /home/ubuntu/admin_token.txt
+
+curl --location --globoff 'http://10.20.0.2:11010/v1/hypervisors/' \
+    --header 'Content-Type: application/json' \
+    --header "Authorization: Bearer $ADMIN_TOKEN" \
+    --data '{
+        "driver_spec": {
+            "driver": "libvirt",
+            "iface_mtu": 1500,
+            "network_type": "network",
+            "network": "genesis-core-net",
+            "storage_pool": "rpool",
+            "connection_uri": "qemu+tcp://10.20.0.1/system",
+            "machine_prefix": "dev-"
+        },
+        "avail_cores": 10,
+        "avail_ram": 15000,
+        "all_cores": 24,
+        "all_ram": 22000,
+        "status": "ACTIVE"
+    }'
+
+# create node example
+# curl --location 'http://10.20.0.2:11010/v1/nodes/' \
+# --header 'Content-Type: application/json' \
+# --header "Authorization: Bearer $ADMIN_TOKEN" \
+# --data '{
+#     "name": "test",
+#     "project_id": "5d72ca4a-c053-4b93-b52a-c20ad9c37be4",
+#     "root_disk_size": 5,
+#     "cores": 1,
+#     "ram": 2048,
+#     "image": "http://10.130.0.1:8080/genesis-base.raw"
+# }'
+
+sudo systemctl restart netfilter-persistent.service
+
+# Make it unsafe but usable for newbies
+sudo passwd -u ubuntu
+echo "ubuntu:ubuntu" | sudo chpasswd
+sudo rm /etc/ssh/sshd_config.d/60-cloudimg-settings.conf
+
+#Do so only once
+sudo rm /etc/cron.d/core_bootstrap
---
+++ genesis/images/install.sh
@@ -0,0 +1,119 @@
+#!/usr/bin/env bash
+
+# Copyright 2025 Genesis Corporation
+#
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+set -eu
+set -x
+set -o pipefail
+
+EL_PATH="/opt/stand"
+
+
+# Optimize apt
+echo 'APT::Install-Recommends "false";' | sudo tee -a /etc/apt/apt.conf.d/00genesis.conf > /dev/null
+echo 'APT::Install-Suggests "false";' | sudo tee -a /etc/apt/apt.conf.d/00genesis.conf > /dev/null
+sudo apt-get update
+sudo apt-get install python3.12-venv yq -y
+
+# Access
+echo "ubuntu:ubuntu" | sudo chpasswd
+sudo rm /etc/ssh/sshd_config.d/60-cloudimg-settings.conf
+sudo yq -yi '.system_info.default_user.lock_passwd |= false' /etc/cloud/cloud.cfg
+
+# Common optimizations for FS and RAM usage
+sudo zfs set compression=zstd rpool
+sudo zfs set sync=disabled rpool
+sudo zfs create rpool/opt
+sudo mv /opt_orig/stand /opt
+sudo rm -rf /opt_orig
+
+sudo mkdir -p /etc/docker/
+echo '{"storage-driver": "zfs"}' | sudo tee /etc/docker/daemon.json
+
+# Reset cloud-init, ubuntu-zfs image have it
+sudo cloud-init clean --log --seed
+sudo rm /etc/netplan/50-cloud-init.yaml
+echo "datasource_list: [ None ]" | sudo tee /etc/cloud/cloud.cfg.d/99_overrides.cfg
+
+# zram
+sudo apt-get update
+sudo apt-get install -y zram-tools linux-modules-extra-$(uname -r)
+echo "ALGO=zstd" | sudo tee -a /etc/default/zramswap > /dev/null
+echo "PERCENT=20" | sudo tee -a /etc/default/zramswap > /dev/null
+sudo systemctl enable zramswap
+sudo systemctl start zramswap
+
+sudo apt-get update
+sudo apt install qemu-guest-agent bridge-utils qemu-kvm libvirt-daemon-system libvirt-dev mkisofs net-tools libvirt-daemon-driver-storage-zfs dnsmasq qemu-system-modules-spice iptables-persistent -y
+
+
+# libvirt install breaks dns, fix it temporarily
+sudo resolvectl dns ens4 1.1.1.1
+
+
+cat | sudo tee -a /etc/libvirt/libvirtd.conf > /dev/null <<EOL
+listen_tcp = 1
+listen_addr = "0.0.0.0"
+auth_tcp = "none"
+EOL
+
+sudo systemctl stop libvirtd
+sudo systemctl enable libvirtd-tcp.socket
+sudo systemctl start libvirtd-tcp.socket
+sudo systemctl start libvirtd
+
+# Prepare storage
+zfs create rpool/disks
+virsh pool-define-as --name rpool --source-name rpool/disks --type zfs
+virsh pool-start rpool
+
+
+# genesis-core itself
+cd /opt
+sudo chown ubuntu ./ -R
+python3 -m venv .venv
+source .venv/bin/activate
+pip install genesis-devtools
+
+cat | sudo tee /etc/iptables/rules.v4 > /dev/null <<EOL
+*filter
+-A FORWARD -d 10.20.0.2/32 -o virbr1 -p tcp -m tcp --dport 11010 -j ACCEPT
+-A FORWARD -d 10.20.0.2/32 -o virbr1 -p tcp -m tcp --dport 5300 -j ACCEPT
+-A FORWARD -d 10.20.0.2/32 -o virbr1 -p udp -m udp --dport 5300 -j ACCEPT
+COMMIT
+*nat
+-A PREROUTING -i enp1s0 -p tcp -m tcp --dport 11010 -j DNAT --to-destination 10.20.0.2:11010
+-A PREROUTING -i enp1s0 -p tcp -m tcp --dport 53 -j DNAT --to-destination 10.20.0.2:5300
+-A PREROUTING -i enp1s0 -p udp -m udp --dport 53 -j DNAT --to-destination 10.20.0.2:5300
+-A POSTROUTING -s 10.20.0.0/24 -o enp1s0 -j MASQUERADE
+COMMIT
+EOL
+
+cat | sudo tee -a /etc/sysctl.conf > /dev/null <<EOL
+net.ipv4.ip_forward=1
+EOL
+
+echo "@reboot ubuntu ${EL_PATH}/genesis/images/bootstrap.sh 2>&1 | logger -t genesis_bootstrap" | sudo tee /etc/cron.d/core_bootstrap > /dev/null
+
+# Minimize image size
+sudo apt-get clean
+sudo rm -rf /var/lib/apt/lists/*
+sudo sync
+sudo zpool sync
+sudo zpool trim -w rpool
+sudo echo '0' | sudo tee /sys/module/zfs/parameters/zfs_initialize_value > /dev/null
+sudo zpool initialize -w rpool

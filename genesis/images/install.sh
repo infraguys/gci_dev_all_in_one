b571bba38ee8b40a0fbc318f208a71e05aac6f46
George Melikov <mail@gmelikov.ru>
2026-04-06 10:24:14 +0300

    Use lz4

--- genesis/images/install.sh
+++ genesis/images/install.sh
@@ -66,7 +66,7 @@ sudo systemctl start libvirtd-tcp.socket
 sudo systemctl start libvirtd
 
 # Prepare storage
-zfs create -o compression=zstd -o volmode=dev rpool/disks
+zfs create -o compression=lz4 -o volmode=dev rpool/disks
 virsh pool-define-as --name rpool --source-name rpool/disks --type zfs
 virsh pool-start rpool
 
